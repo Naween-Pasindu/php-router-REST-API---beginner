7617b4135ff251b9fe6a738123f8d3293ca3dc4b
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-09-28 12:47:28 +0530

    bug fixed

--- public/Views/InventoryManager/index.php
+++ public/Views/InventoryManager/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <link rel="stylesheet" href="/simple/assets/css/main.css">
+    <link rel="stylesheet" href="/<?php echo baseUrl; ?>/public/assets/css/main.css">
 <style>
     /*
 body{
