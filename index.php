dc0dae03fdaddfc10af3d48533dceecc4f98b372
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-10-04 12:47:49 +0530

    test - 4 try to identify root

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-define("baseUrl","");
+define("baseUrl","/home/site/wwwroot");
 
 if($_SERVER['REQUEST_METHOD'] === 'PUT' || $_SERVER['REQUEST_METHOD'] === 'DELETE' || $_SERVER['REQUEST_METHOD'] === 'POST'){
     require  "app/index.php";
 }else{
     require  "public/index.php";
 } 
-exit();
 //require  "app/index.php";
