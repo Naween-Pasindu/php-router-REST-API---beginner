7d25aecfb0e93f28a43671f25c9d675b419f5625
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-10-04 12:54:30 +0530

    test 7

--- public/Controller/Router.php
+++ public/Controller/Router.php
@@ -19,6 +19,7 @@ class Router{
 
     public function __construct(){
         $url = $this->getUrl();
+        print_r($url);
         if(array_key_exists($url[0],Router::$defaultController) && count($url)==1){
             $this->currentController = 'public/Views/'.Router::$defaultController[$url[0]];
         }else if(array_key_exists($url[0],Router::$defaultController)){
