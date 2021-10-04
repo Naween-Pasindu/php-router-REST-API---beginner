8680124fa3417401a62109794f393c1ce139ca9f
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-10-04 12:52:19 +0530

    test 6

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
-/*session_start();
-define("baseUrl","/home/site/wwwroot");
+session_start();
+define("baseUrl","");
 
 if($_SERVER['REQUEST_METHOD'] === 'PUT' || $_SERVER['REQUEST_METHOD'] === 'DELETE' || $_SERVER['REQUEST_METHOD'] === 'POST'){
     require  "app/index.php";
 }else{
     require  "public/index.php";
 } 
-*/
-phpinfo();
 //require  "app/index.php";
