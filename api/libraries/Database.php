a2cf537147d40a0d16eda242ecdcd86bee24c6e7
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-11-01 20:04:49 +0530

    folder renamed and documentation template added

---
+++ api/libraries/Database.php
@@ -0,0 +1,42 @@
+<?php
+class Database{
+	private $_connection;
+	private static $_instance; //The single instance
+	private $_host = DB_HOST;
+	private $_username = DB_USER;
+	private $_password = DB_PASS;
+	private $_database = DB_NAME;
+
+	public static function getInstance() {
+		if(!self::$_instance) { // If no instance then make one
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	private function __construct() {
+		$this->_connection = new mysqli($this->_host, $this->_username, 
+			$this->_password, $this->_database);
+			if (!$this->_connection)
+			{
+				error_reporting(0);
+				die("Error: Unable to connect to MySQL." . PHP_EOL);
+			}
+		if($this->_connection->connect_errno) {
+			//trigger_error("Failed to conencto to MySQL: " . $this->_connection->connect_errno,E_USER_ERROR);
+			echo json_encode(array("code"=>$this->_connection->connect_errno));
+			exit();
+		}
+	}
+
+	private function __clone() { }
+
+	public function getConnection() {
+		return $this->_connection;
+	}
+
+/*	public function backlog() {
+
+	}
+*/
+}
--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("public/Controller/Router.php");
-
-$control = new Router();
